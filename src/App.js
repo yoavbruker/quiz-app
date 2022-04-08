bd52bb24cc4d3ca735ebf02b6c6c5bed570b09f2
yoavbruker <75201251+yoavbruker@users.noreply.github.com>
2022-04-08 16:29:29 +0300

    move logic from app to question game panel component

--- src/App.js
+++ src/App.js
@@ -1,43 +1,13 @@
-import logo from "./logo.svg";
+import React from "react";
+import QuestionGamePanel from "./components/question-game-panel/question-game-panel";
 import "./App.css";
-import Question from "./components/question/question";
-import React, { useEffect, useState } from "react";
 
-function App() {
-  const [score, setScore] = useState(0);
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [allQuestions, setAllQuestions] = useState([]);
-
-  useEffect(() => {
-    const getAllQuestions = async () => {
-      console.log("hello");
-      try {
-        const questionsApiResponse = await fetch(
-          "https://opentdb.com/api.php?amount=100"
-        );
-        console.log(questionsApiResponse);
-        const allQuestionsResponseJson = await questionsApiResponse.json();
-        console.log(allQuestionsResponseJson);
-        setAllQuestions(allQuestionsResponseJson.results);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    getAllQuestions();
-  }, [setAllQuestions]);
-
-  const result = allQuestions[currentQuestion];
-
+const App = () => {
   return (
     <div className="App">
-      <Question
-        question={result?.question}
-        rightAnswer={result?.correct_answer}
-        wrongAnswers={result?.incorrect_answers}
-      />
+      <QuestionGamePanel />
     </div>
   );
-}
+};
 
 export default App;
