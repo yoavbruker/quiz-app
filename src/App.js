f536aef1464f97b62d3c3fe4a773b7b1ac98b64e
yoavbruker <75201251+yoavbruker@users.noreply.github.com>
2022-04-08 13:33:21 +0300

    temp: checking question component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Question from "./components/question/question";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [allQuestions, setAllQuestions] = useState([]);
+
+  useEffect(() => {
+    const getAllQuestions = async () => {
+      console.log("hello");
+      try {
+        const questionsApiResponse = await fetch(
+          "https://opentdb.com/api.php?amount=100"
+        );
+        console.log(questionsApiResponse);
+        const allQuestionsResponseJson = await questionsApiResponse.json();
+        console.log(allQuestionsResponseJson);
+        setAllQuestions(allQuestionsResponseJson.results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getAllQuestions();
+  }, [setAllQuestions]);
+
+  const result = allQuestions[currentQuestion];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Question
+        question={result?.question}
+        rightAnswer={result?.correct_answer}
+        wrongAnswers={result?.incorrect_answers}
+      />
     </div>
   );
 }
